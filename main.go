a329cdaa8cb2f927f8e0843844786bb595efc48a
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2024-03-15 12:32:07 +0300

    first approach to pass the tests

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
+	nums := []int{3, 2, 4}
+	target := 6
 
+	fmt.Println("the positions", twoSum(nums, target))
 }
-func twoSum(nums []int, target int) []int {
 
-	return nil
+func twoSum(nums []int, target int) []int {
+	res := []int{}
+
+	for i, num := range nums {
+		for j, num2 := range nums {
+			//a number cannot be repeated
+			if i == j {
+				continue
+			}
+
+			sum := num + num2
+
+			if sum == target {
+				res = append(res, i, j)
+
+				break
+			}
+		}
+
+		if len(res) > 0 {
+			break
+		}
+	}
+
+	return res
 }
